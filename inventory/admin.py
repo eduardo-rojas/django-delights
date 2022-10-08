51ad6356eb708e65c462ea45e1c3887ef49443bf
Eduardo Rojas <carlos.edhr@protonmail.com>
2022-10-07 22:13:22 -0700

    Added model 'MenuItem', registered new model to admin site, included slug field to both models 'MenuItem' and 'Ingredients', configured slugs as prepopulated fields. Successfully added entries to test database through admin site. Installed pillow to handle images.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import Ingredient
+from .models import Ingredient, MenuItem
 
 # Register Ingredient model to admin site
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = [
-        'name' , 
-        'quantity' , 
-        'unit_price' , 
-        'unit' 
-    ]
+    list_display = ['name' , 'slug', 'quantity' , 
+        'unit_price' , 'unit' ]
+    prepopulated_fields= {'slug': ('name', )}
+
+@admin.register(MenuItem)
+class MenuItemadmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'slug',
+                    'created', 'updated']
+    list_filter = ['created', 'updated']
+    list_editable = ['price']
+    prepopulated_fields= {'slug': ('title', )}
--- inventory/models.py
+++ inventory/models.py
@@ -1,24 +1,55 @@
 from django.db import models
 
-# Ingredient model
+# Model: Ingredient
 class Ingredient(models.Model):
-
+    # Fields
     name = models.CharField(max_length=30)
     quantity = models.FloatField()
-    unit_price = models.FloatField()
+    # Using DecimalField instead of FloatField for monetary fields
+    # in order to avoid rounding issues
+    unit_price = models.DecimalField(max_digits=10, 
+                                    decimal_places=2)
+    slug = models.SlugField(max_length=200)
     unit = models.CharField(max_length=30)
 
-    # Meta attributes to order ingredients by name. Database Index for better performance
+    # Meta class with attributes to order ingredients by name
+    # Database Index to improve query performance
     class Meta:
         ordering = [ 'name']
         indexes = [
             models.Index(fields=['name'])
         ]
     
-    # String method returns ingredient instance name
+    # String method returns readable ingredient instance name
     def __str__(self):
         return self.name
     
+# Model: MenuItem
+class MenuItem(models.Model):
+    # Fields
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, 
+                                decimal_places=2)
+    slug = models.SlugField(max_length=200)
+    image = models.ImageField(upload_to='inventory/%Y/%m/%d',
+                            blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # Meta class. Order by title
+    class Meta:
+        ordering = ['title']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['title']), 
+            models.Index(fields=['-created'])
+        ]
+    
+    # String format method
+    def __str__(self):
+        return self.title
+    
 
 
+    
 

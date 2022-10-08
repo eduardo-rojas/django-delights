f44d2e4ad99f646a56000d0f0d40b5f5c87fd2b4
Eduardo Rojas <carlos.edhr@protonmail.com>
2022-10-08 00:12:57 -0700

    Created models: 'RecipeRequirements', 'Purchase' and 'Profile'. 'Profile' model extends django User model. Registered models to admin site.

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient, MenuItem
+from .models import Ingredient, MenuItem, RecipeRequirement, Purchase, Profile
 
 # Register Ingredient model to admin site
 @admin.register(Ingredient)
@@ -15,3 +15,24 @@ class MenuItemadmin(admin.ModelAdmin):
     list_filter = ['created', 'updated']
     list_editable = ['price']
     prepopulated_fields= {'slug': ('title', )}
+
+@admin.register(RecipeRequirement)
+class RecipeRequirementAdmin(admin.ModelAdmin):
+    list_display = ['menu_item', 'ingredient', 'quantity',
+                     'slug', 'created']
+    list_filter = ['quantity', 'created']
+    prepopulated_fields = {'slug':('menu_item', 'ingredient')}
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ['menu_item', 'timestamp', 'total_price', 'slug']
+    list_filter = ['menu_item', 'timestamp','total_price']
+    prepopulated_fields = {'slug': ('id', 'timestamp' )}
+
+@admin.register(Profile)
+class Profile(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'created', 
+                    'updated', 'purchase_dispatched']
+    list_filter = ['user', 'date_of_birth']
+    prepopulated_fields = {'slug': ('user',  )}
--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Model: Ingredient
 class Ingredient(models.Model):
@@ -49,6 +50,71 @@ class MenuItem(models.Model):
     def __str__(self):
         return self.title
     
+# Model: RecipeRequirement
+class RecipeRequirement(models.Model):
+    # Fields
+    quantity = models.FloatField()
+    menu_item = models.ForeignKey(MenuItem,
+                                on_delete=models.CASCADE,   
+                                related_name="menuitem_recipes")
+    ingredient = models.ForeignKey(Ingredient,  
+                                on_delete=models.CASCADE,
+                                related_name="ingredient_recipes")
+    slug = models.SlugField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    # Meta class. Order by quantity
+    class Meta:
+        ordering = ['quantity']
+        indexes = [
+            models.Index(fields=['menu_item']),
+            models.Index(fields=['ingredient'])
+        ] 
+    
+    # String format method
+    def __str__(self):
+        return f'Recipe for {self.menu_item} with ingredient {self.ingredient}'
+    
+# Model: Purchase
+class Purchase(models.Model):
+    # Fields
+    menu_item = models.ForeignKey(MenuItem,
+                                on_delete=models.CASCADE,
+                                related_name='purchase_menuitems')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10,
+                                    decimal_places=2)
+    slug = models.SlugField(max_length=200)
+
+    # Meta class. Order by total_price
+    ordering = ['total_price']
+    indexes = [
+        models.Index(fields=['menu_item']),
+        models.Index(fields=['total_price']),
+    ]
+
+    # String format method
+    def __str__(self):
+        return f'Purchase with id#: {self.id} - Date:{self.timestamp} - Total: ${self.total_price}'
+    
+# Model: Profile
+class Profile(models.Model):
+    # Fields
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to="inventory/%Y/%m/%d")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    purchase_dispatched = models.ForeignKey(Purchase,
+                                        on_delete=models.CASCADE,
+                                        related_name='profile_purchase')
+    slug = models.SlugField(max_length=200)
+    
+    def __str__(self):
+        return f'Profile of {self.username}'
+    
+
 
 
     

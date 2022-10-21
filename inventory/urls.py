979f4649a90e76e5996f02641a0df78167c358c1
Eduardo Rojas <carlos.edhr@protonmail.com>
2022-10-20 18:32:01 -0700

    Added IngredientUpdate and IngredientDelete classes to views. CRUD functionality completed for model: Ingredient.

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # Ingredients
     path("ingredient/list", views.IngredientList.as_view(), name="ingredientlist"),
     path("ingredient/create", views.IngredientCreate.as_view(), name="ingredientcreate" ),
-
+    path("ingredient/update/<pk>", views.IngredientUpdate.as_view(), name="ingredientupdate"),
+    path("ingredient/delete/<pk>", views.IngredientDelete.as_view(), name="ingredientdelete"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -6,14 +6,27 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 
 
+# Ingredient CRUD class-based views
 
 class IngredientList(ListView):
     model = Ingredient
-    template_name = "inventory/ingredient_list.html"
+    template_name = "inventory/ingredient/ingredient_list.html"
 
 class IngredientCreate(CreateView):
     model = Ingredient
-    template_name = "inventory/ingredient_create_form.html"
+    template_name = "inventory/ingredient/ingredient_create_form.html"
     fields = ["name", "quantity", "unit_price",  "unit"]
     success_url = "/inventory/ingredient/list"
+
+class IngredientUpdate(UpdateView):
+    model = Ingredient
+    template_name = "inventory/ingredient/ingredient_update_form.html"
+    fields = ["name", "quantity", "unit_price",  "unit"]
+    success_url = "/inventory/ingredient/list"
+
+class IngredientDelete(DeleteView):
+    model = Ingredient
+    template_name = "inventory/ingredient/ingredient_delete_form.html"
+    success_url = "/inventory/ingredient/list"
+
 
